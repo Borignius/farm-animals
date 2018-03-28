e34e3a589f728099d8b17f6811775cafe36fbd5e
Kyle Coberly <kyle.coberly@gmail.com>
2018-03-28 10:55:30 -0600

    Initial commit

---
+++ migrations/20180328103747_animal.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("animal", animal => {
+        animal.increments();
+        animal.string("name");
+        animal.string("breed");
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("animal");
+};
---
+++ seeds/01-animal.js
@@ -0,0 +1,15 @@
+exports.seed = function(knex, Promise) {
+    return knex("animal").del().then(() => {
+        return knex("animal").insert([{
+            id: 1,
+            name: "Wilbur",
+            breed: "Pig"
+        },{
+            id: 2,
+            name: "Glooey",
+            breed: "Horse"
+        }]);
+    }).then(() => {
+        return knex.raw("ALTER SEQUENCE animal_id_seq RESTART WITH 3");
+    });
+};
